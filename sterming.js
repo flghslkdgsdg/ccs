97c6b43dcbc12916f2498685c991efed83b08f39
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-11-15 21:34:41 +0200

    Update sterming.js

--- sterming.js
+++ sterming.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const adminprefix = "+";
-const devs = ['489342754887827487'];
+const devs = ['427611667631964180']; 
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
