0d86327ee9bb27508d9b5df5a66e009450cbc46a
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-11-15 21:41:34 +0200

    Update sterming.js

--- sterming.js
+++ sterming.js
@@ -1,34 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const adminprefix = "+";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`الزمن لآيغير احد ,الزمن يكشف كل انسآآن على حقيقتهه`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-const adminprefix = "+";
 const devs = ['427611667631964180'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
