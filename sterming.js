c1a732c9c94a329f8c525b37e5805702533ad9f8
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-11-16 18:45:42 +0200

    Update sterming.js

--- sterming.js
+++ sterming.js
@@ -1,70 +1,45 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const developers = ['463041270277996554'];
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`L`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  client.user.setGame(`~`,'https://www.twitch.tv/Mdax')
+  client.user.setStatus("idle")
 });
 
-const adminprefix = "+";
-const devs = ['427611667631964180'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('Lwt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** ${argresult} \ ** `)
 } else 
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-if (message.content.startsWith(adminprefix + 'setstreem')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+if (message.content.startsWith('Lls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** ${argresult} \ ** `)
+} else 
+if (message.content.startsWith('Lst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Mdax");
+  client.user.setStatus("idle")
+   message.channel.send(` ** ${argresult} \ ** `)
 }
-
 });
 
-client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setWatch")) {
-       if(message.author.id !== '427611667631964180') return;
-       client.user.setActivity(args,{type: 'WATCHING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
-client.on('message',function(message) {
-    let args = message.content.split(" ").slice(1).join(" ");
-   if(message.content.startsWith(adminprefix + "setListen")) {
-       if(message.author.id !== '427611667631964180') return;
-       client.user.setActivity(args,{type: 'LISTENING'});
-       message.channel.send("**- :white_check_mark: Done!,**");
-   } 
-});
+client.on('message', message => {
+        if (!developers.includes(message.author.id)) return;
+  if (message.content === '0..') {
+  let channel = client.channels.get('511744532992229377');
 
+  channel.join()
+  .then(connection => console.log('Connected'))
+  .catch(console.error);
+    
+  }
+          
+});
 client.login(process.env.BOT_TOKEN);
