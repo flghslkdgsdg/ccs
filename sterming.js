9dd3564ee3633421acf27adf7f91eb6a0aa50cfd
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-11-16 05:57:42 +0200

    Update sterming.js

--- sterming.js
+++ sterming.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Time does not change one, time reveals every aspect of its reality`,"http://twitch.tv/S-F")
+client.user.setGame(`Time does not change one, time reveals every person to his truth`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
