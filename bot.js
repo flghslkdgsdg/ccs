0daa4c97ccf52735e3fc8d25a7fb1efdd7a2dc70
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-11-17 15:58:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,28 +28,3 @@ client.user.setGame(`Nothing, ☕💙`,"https://www.twitch.tv/love")
   console.log('')
 });
 
-
-
-
-
-
-client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = [`Nothing, ☕💙`];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`https://www.twitch.tv/love`);    
-}, ms);    
-    
-});
-
-
-client.login(process.env.BOT_TOKEN);
