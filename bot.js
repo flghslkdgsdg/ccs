cf8a7e680506db67858a4bd0d52f79f8845e99c5
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2018-11-21 00:42:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,55 +1,45 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const developers = ['412373041675370497'];
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`i Love you, 💜`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  client.user.setGame(`~`,'https://www.twitch.tv/Mdax')
+  client.user.setStatus("idle")
 });
 
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
 
+if (message.content.startsWith('Mwt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** ${argresult} \ ** `)
+} else 
+if (message.content.startsWith('Mls')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+  client.user.setStatus("idle")
+    message.channel.send(` ** ${argresult} \ ** `)
+} else 
+if (message.content.startsWith('Mst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Mdax");
+  client.user.setStatus("idle")
+   message.channel.send(` ** ${argresult} \ ** `)
+}
+});
 
+client.on('message', message => {
+        if (!developers.includes(message.author.id)) return;
+  if (message.content === '0..') {
+  let channel = client.channels.get('511744532992229377');
 
-
-
-client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = [`i Love you, 💙`,"i Love you, 🖤 ,"];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`http://twitch.tv/S-F`);    
-}, ms);    
+  channel.join()
+  .then(connection => console.log('Connected'))
+  .catch(console.error);
     
+  }
+          
 });
-
-
 client.login(process.env.BOT_TOKEN);
